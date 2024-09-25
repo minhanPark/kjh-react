9a5c0bc591710093053b570d29c51f29a5eb9e70
Running water <alsgks1218@naver.com>
2024-09-25 23:13:23 +0900

    버튼 컴포넌트 분리

--- 1-react/src/App.jsx
+++ 1-react/src/App.jsx
@@ -1,3 +1,47 @@
-const App = () => <>1-react</>;
+import Button from "./components/Button";
+
+const App = () => (
+  <div className="ProductPage">
+    <div className="Page">
+      <header>
+        <h1>메뉴</h1>
+      </header>
+      <main>
+        <ul>
+          <li>
+            <div className="ProductItem">
+              <div className="description">
+                <h2>고소한 바질 파스타</h2>
+                <div className="">6,000</div>
+                <Button
+                  styleType="brand"
+                  onClick={() => console.log("주문하기 클릭")}
+                >
+                  주문하기
+                </Button>
+              </div>
+              <div className="thumbnail">
+                <img
+                  src="./images/menu-고소한바질파스타.jpg"
+                  alt="바질 파스타"
+                />
+              </div>
+            </div>
+          </li>
+        </ul>
+      </main>
+      <footer>
+        <nav className="Navbar">
+          <a href="#" className="active">
+            메뉴목록
+          </a>
+          <a href="#" className="">
+            주문목록
+          </a>
+        </nav>
+      </footer>
+    </div>
+  </div>
+);
 
 export default App;
