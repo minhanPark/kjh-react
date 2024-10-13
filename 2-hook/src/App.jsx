ac1745c80573cff8e127cb15cf1d9f9211ec92e5
Running water <alsgks1218@naver.com>
2024-10-13 13:37:30 +0900

    비동기 때 상태값 바뀌는 부분 테스트

--- 2-hook/src/App.jsx
+++ 2-hook/src/App.jsx
@@ -1,3 +1,36 @@
-const App = () => <>2-hook</>;
+import React from "react";
+
+class Contract extends React.Component {
+  sign() {
+    const name = this.props.name;
+    setTimeout(() => console.log("서명인: ", name), 3000);
+  }
+  render() {
+    return <button onClick={this.sign.bind(this)}>서명</button>;
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: "사용자1" };
+  }
+
+  handleChange(e) {
+    this.setState({ name: e.target.value });
+  }
+
+  render() {
+    return (
+      <>
+        <select value={this.state.name} onChange={this.handleChange.bind(this)}>
+          <option value="사용자1">사용자1</option>
+          <option value="사용자2">사용자2</option>
+        </select>
+        <Contract name={this.state.name} />
+      </>
+    );
+  }
+}
 
 export default App;
