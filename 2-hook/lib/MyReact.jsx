5e950d125f7d8f6118528a8ba11f43c387d232e1
Running water <alsgks1218@naver.com>
2024-10-14 22:52:20 +0900

    useState 구현

---
+++ 2-hook/lib/MyReact.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const MyReact = (function MyReact() {
+  const memorizedStates = [];
+  const isInitialized = [];
+  let cursor = 0;
+
+  function useState(initialValue = "") {
+    const { forceUpdate } = useForceUpdate();
+
+    if (!isInitialized[cursor]) {
+      memorizedStates[cursor] = initialValue;
+      isInitialized[cursor] = true;
+    }
+
+    const state = memorizedStates[cursor];
+
+    const setStateAt = (_cursor) => (nextState) => {
+      if (state === nextState) return;
+      memorizedStates[_cursor] = nextState;
+      forceUpdate();
+    };
+
+    const setState = setStateAt(cursor);
+
+    cursor = cursor + 1;
+
+    return [state, setState];
+  }
+
+  function useForceUpdate() {
+    const [value, setValue] = React.useState(1);
+    const forceUpdate = () => {
+      setValue(value + 1);
+      cursor = 0;
+    };
+    return { forceUpdate };
+  }
+
+  return { useState };
+})();
+
+export default MyReact;
--- 2-hook/src/App.jsx
+++ 2-hook/src/App.jsx
@@ -1,36 +1,46 @@
-import React from "react";
+//import React from "react";
+import MyReact from "../lib/MyReact";
 
-class Contract extends React.Component {
-  sign() {
-    const name = this.props.name;
-    setTimeout(() => console.log("서명인: ", name), 3000);
-  }
-  render() {
-    return <button onClick={this.sign.bind(this)}>서명</button>;
-  }
+function NameField() {
+  const [firstName, setFirstName] = MyReact.useState("사용자1");
+  const [lastName, setLastName] = MyReact.useState("박");
+
+  const handleChangeFirstName = (e) => {
+    setFirstName(e.target.value);
+  };
+  const handleChangeLastName = (e) => {
+    setLastName(e.target.value);
+  };
+  return (
+    <>
+      <input type="text" value={firstName} onChange={handleChangeFirstName} />
+      <input type="text" value={lastName} onChange={handleChangeLastName} />
+    </>
+  );
 }
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { name: "사용자1" };
+//   }
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { name: "사용자1" };
-  }
+//   handleChange(e) {
+//     this.setState({ name: e.target.value });
+//   }
 
-  handleChange(e) {
-    this.setState({ name: e.target.value });
-  }
+//   render() {
+//     return (
+//       <>
+//         <select value={this.state.name} onChange={this.handleChange.bind(this)}>
+//           <option value="사용자1">사용자1</option>
+//           <option value="사용자2">사용자2</option>
+//         </select>
+//         <Contract name={this.state.name} />
+//       </>
+//     );
+//   }
+// }
 
-  render() {
-    return (
-      <>
-        <select value={this.state.name} onChange={this.handleChange.bind(this)}>
-          <option value="사용자1">사용자1</option>
-          <option value="사용자2">사용자2</option>
-        </select>
-        <Contract name={this.state.name} />
-      </>
-    );
-  }
-}
+// export default App;
 
-export default App;
+export default () => <NameField />;
